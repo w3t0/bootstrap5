35eb3aee7abd9a5caa4581c71a08a159358f4748
w3t0 <156255433+w3t0@users.noreply.github.com>
2024-01-11 02:45:49 +0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,55 @@
+window.addEventListener('DOMContentLoaded', event => {
+
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    navbarShrink();
+
+    document.addEventListener('scroll', navbarShrink);
+
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+
+    const form = document.getElementById('contactForm');
+    const successToastEl = document.getElementById('successToast');
+
+    const successToast = new bootstrap.Toast(successToastEl);
+
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        successToast.show();
+    });
+
+    const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    tooltipTriggerList.forEach(function (tooltipTriggerEl) {
+        new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+});
